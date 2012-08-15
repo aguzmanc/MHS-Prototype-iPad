771099c1671502602a5cbc420ef1781ff5c41cef
Giancarlo <giancarlo@Giancarlos-Mac.local>
2012-08-14 22:36:20 -0500

    ultima actualizacion login
    se inserto json

--- Source/Globals.h
+++ Source/Globals.h
@@ -1,6 +1,12 @@
 #ifndef MHS_Prototype_Globals_h
 #define MHS_Prototype_Globals_h
 
-
+// Quark Social Services
+#define REGISTER_USER_SERVICE_REQUEST_PAGE @"http://www.quarksocial.net/mobile_health/login.php"
+#define IS_USER_REGISTERED_SERVICE_REQUEST_PAGE @"http://www.quarksocial.com/Axe/Apps/Deseo/App_Deseo/Servicios/IsUserRegisteredService.php"
+#define GET_DESIRE_LIST_SERVICE_REQUEST_PAGE @"http://www.quarksocial.com/Axe/Apps/Deseo/App_Deseo/Servicios/GetDesireListService.php"
+#define SEND_DESIRE_SERVICE_REQUEST_PAGE @"http://www.quarksocial.com/Axe/Apps/Deseo/App_Deseo/Servicios/SendDesireService.php"
 
 #endif
+
+
--- Source/Logic/Logic.h
+++ Source/Logic/Logic.h
@@ -1,5 +1,7 @@
 #import <Foundation/Foundation.h>
 
+
+
 @protocol ViewChangerDelegate;
 
 @interface Logic : NSObject
