5b4ab290316a3ccd423ea0f6825bca4d87b15c10
Giancarlo <giancarlo.delgadillo@wiserkronox.com>
2012-08-18 11:14:54 -0500

    ultima actualizacion de gian

---
+++ Source/Services/ProgressService.h
@@ -0,0 +1,13 @@
+//
+//  ProgressService.h
+//  MHS Prototype
+//
+//  Created by Giancarlo on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProgressService : NSObject
+
+@end
---
+++ Source/Services/ProgressService.m
@@ -0,0 +1,13 @@
+//
+//  ProgressService.m
+//  MHS Prototype
+//
+//  Created by Giancarlo on 8/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ProgressService.h"
+
+@implementation ProgressService
+
+@end
