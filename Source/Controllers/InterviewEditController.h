f2147b26c845e70b7d5ab7478a98c32193d86510
Giancarlo <giancarlo.delgadillo@wiserkronox.com>
2012-08-19 18:22:38 -0500

    cambios

---
+++ Source/Controllers/InterviewEditController.h
@@ -0,0 +1,13 @@
+//
+//  InterviewEditController.h
+//  MHS Prototype
+//
+//  Created by Giancarlo on 8/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InterviewEditController : UIViewController
+
+@end
